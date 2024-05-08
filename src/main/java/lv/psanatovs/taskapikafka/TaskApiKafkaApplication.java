2edc5f08d3758851590009c088c4ca64b50e3642
Ma Nul <psanatovs@gmail.com>
2024-05-09 00:36:46 +0300

    Initial commit

---
+++ src/main/java/lv/psanatovs/taskapikafka/TaskApiKafkaApplication.java
@@ -0,0 +1,13 @@
+package lv.psanatovs.taskapikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApiKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lv/psanatovs/taskapikafka/TaskApiKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.psanatovs.taskapikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
